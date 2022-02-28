b3628d142b05d39c4d4f7ea19375ad63c26ef807
LVD <vdvlorenz@gmail.com>
2022-02-28 13:45:02 +0100

    fix recv

--- flame.sh
+++ flame.sh
@@ -4,5 +4,4 @@
 # PATH=$PATH:~/.cargo/bin
 
 mkdir -p flame
-#sudo bash -i -c 'cd flame && flamegraph -o graph.svg ../test'
-sudo bash -i -c 'cd flame && flamegraph -o graph.svg -p 1602207'
+sudo bash -i -c 'cd flame && flamegraph -o graph.svg ../test'
