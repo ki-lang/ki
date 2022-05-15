11b424f25f0cc8d8e42bc8f84b54b7d37518f322
LVD <vdvlorenz@gmail.com>
2022-05-15 23:24:42 +0200

    adjust macos install

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ if [ "$UNAME" == "Linux" ] ; then
 	sudo cp ./ki /usr/bin/
 elif [ "$UNAME" == "Darwin" ] ; then
 	echo "# macOS install"
-	sudo cp ./ki /usr/bin/
+	sudo mkdir -p /usr/local/bin
+	sudo cp ./ki /usr/local/bin/
 else
 	echo "Install script currently doesnt support: $UNAME"
 fi
