eeeb6684e96326e9e1717d8ab233e99194e485b8
ctxcode <vdvlorenz@gmail.com>
2024-01-25 00:30:26 +0100

    remove IPO.h

--- src/build/stage-5-link.c
+++ src/build/stage-5-link.c
@@ -12,7 +12,6 @@
 #include <llvm-c/Support.h>
 #include <llvm-c/Target.h>
 #include <llvm-c/TargetMachine.h>
-#include <llvm-c/Transforms/IPO.h>
 #include <llvm-c/Transforms/PassBuilder.h>
 #include <llvm-c/Transforms/PassManagerBuilder.h>
 #include <llvm-c/Transforms/Scalar.h>
